7b2d779584a6dcaea3a90bbe8e383118c58bcefa
vaishnavi <vaishu.srini15@gmail.com>
2023-09-22 17:36:37 -0500

    Overloading methods in compiled time polymorphism

---
+++ src/src/overloading/Addition.java
@@ -0,0 +1,21 @@
+package overloading;
+class AddNum{
+   public int add(int a,int b){
+       return a + b;
+   }
+
+    public int add(int a,int b,int c){
+        return a + b + c;
+    }
+
+
+}
+
+public class Addition {
+    public static void main(String[] args) {
+
+        AddNum obj = new AddNum();
+        System.out.println(obj.add(2,3,4));
+        System.out.println(obj.add(5,10));
+    }
+}
