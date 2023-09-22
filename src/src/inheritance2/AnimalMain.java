51fe4004cbd1435daf8658f1a043432248138410
vaishnavi <vaishu.srini15@gmail.com>
2023-09-22 14:26:54 -0500

    Multilevel Inheritance

--- src/src/inheritance2/AnimalMain.java
+++ src/src/inheritance2/AnimalMain.java
@@ -1,20 +1,28 @@
 package inheritance2;
 
 class Cat{
+    String play = "Ball, climb";
     String food = "Milk, Fish";
     void noise(){
         System.out.println("meaw,meaw");
+        
     }
 }
-class Dog{
+class Dog extends Cat{
+    String breed = "Boxer, Labrador";
     String food = "Bones, Meat";
-    void noise(){
+   void noise(){
         System.out.println("woof, woof");
+        
     }
 }
+class Pet extends Dog{
+    String color = "Black, White, Gray";
 
-
-
+    void action(){
+        System.out.println("Jump, Run, Care");
+    }
+}
 
 
 public class AnimalMain {
@@ -28,5 +36,11 @@ public class AnimalMain {
         Dog d = new Dog();
         System.out.println(d.food);
         d.noise();
+        System.out.println(d.breed);
+
+
+        Pet p = new Pet();
+        System.out.println(p.color);
     }
+
 }
