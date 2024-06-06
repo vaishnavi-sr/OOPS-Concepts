b45bbb2d846f3f06856e9799f38ef1dbb4ec6b9c
vaishnavi <vaishu.srini15@gmail.com>
2024-06-06 15:37:26 -0500

    methods written

--- src/src/classes/Demo.java
+++ src/src/classes/Demo.java
@@ -1,23 +1,55 @@
 package classes;
 
- class Computer{
+ class Computer {
      int a;
-     public int add(int n1, int n2){
-         int result = n1 +n2;
+     private boolean player;
+     public void player() {
+         System.out.println("Playing music....");
+     }
+
+     public String getMePen(int cost) {
+         if (cost >= 10) {
+             return "pen";
+         } else {
+             return "Nothing..";
+         }
+     }
+
+     public int add(int n1, int n2) {
+         int result = n1 + n2;
          return result;
      }
 
-}
-public class Demo {
+     public int sub(int n1, int n2) {
+         if (n1 > n2) {
+             return (n1 - n2);
+         } else {
+             return (n2 - n1);
+         }
 
-    public static void main(String[] args) {
+     }
 
-        int num1 = 24;
-        int num2 = 20;
 
-        Computer c = new Computer();
-        int result1 = c.add(num1,num2);
-        System.out.println(result1);
 
-    }
-}
+ }
+
+     public class Demo {
+
+         public static void main(String[] args) {
+
+             int num1 = 24;
+             int num2 = 28;
+
+             Computer c = new Computer();
+             int result1 = c.add(num1, num2);
+             int result2 = c.sub(num1, num2);
+             System.out.println(result2);
+             System.out.println(result1);
+//       System.out.println(c.player);
+      System.out.println(c.getMePen(5));
+
+         }
+
+
+     }
+
